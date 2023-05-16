558ee3238edcba91cb9cfee5c8a7a2ce996cb179
Lucas Teles <devlucasteles@gmail.com>
2023-05-16 03:04:08 -0300

    feat: :poop: adds new configs to client side that doesnt work to socketio :(

--- windows-installer/src/main.py
+++ windows-installer/src/main.py
@@ -1,27 +1,43 @@
 
-import asyncio
 import socketio
+from scapy.all import *
+from threading import Thread
 
 from identity_service import get_identity
+
+from traffic_analyser import fetch_connections, get_traffic_data, json_serialize_traffic_data, is_program_running
 
 SERVER = 'http://localhost:3333/api?token='
 
 my_identity = None
 
-loop = asyncio.get_event_loop()
-sio = socketio.AsyncClient()
-start_timer = None
+sio = socketio.Client()
+
+
+def send_data(data):
+    sio.emit('send_message', data)
+
+
+def send_data_to_server():
+    print(f'Is program running: {is_program_running}')
+    while is_program_running:
+        data = json_serialize_traffic_data()
+        if data:
+            send_data(data)
+            print("Data sent to server!")
+        print("Sending data to server...")
+        time.sleep(5)
 
 
 @sio.event
-async def connect():
+def connect():
     print('connected to server')
 
 
-async def start_server():
-    connection_string = SERVER + my_identity["token"]
-    await sio.connect(connection_string, transports=['websocket'],)
-    await sio.wait()
+def start_server():
+    connection_string = SERVER + my_identity['token']
+    sio.connect(url=connection_string, transports=['websocket'])
+    sio.wait()
 
 
 def main():
@@ -30,7 +46,15 @@ def main():
     global my_identity
     my_identity = get_identity()
 
-    loop.run_until_complete(start_server())
+    
+
+    # send_data_to_server_thread = Thread(target=send_data_to_server)
+    # send_data_to_server_thread.start()
+
+    # fetch_connections_thread = Thread(target=fetch_connections)
+    # fetch_connections_thread.start()
+    
+    start_server()
 
 
 if __name__ == "__main__":
