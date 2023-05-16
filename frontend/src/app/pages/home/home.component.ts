5077b3d9ea5f1ba02fc803a431d3a9db3381c3c9
joaogoulartt <joaoogoulartt@gmail.com>
2023-05-16 15:49:17 -0300

    feat: :sparkles: adds home page base

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -3,8 +3,76 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  devices = [
+    {
+      deviceId: '1324654136',
+      deviceName: 'Device 1',
+      data: [
+        {
+          title: 'Angular',
+          downloadSize: 22.5,
+          uploadSize: 12.3,
+          upDate: '16/05/2023',
+          upTime: '3123',
+        },
+        {
+          title: 'React',
+          downloadSize: 22.5,
+          uploadSize: 12.3,
+          upDate: '16/05/2023',
+          upTime: '3123',
+        },
+        {
+          title: 'Java',
+          downloadSize: 22.5,
+          uploadSize: 12.3,
+          upDate: '16/05/2023',
+          upTime: '3123',
+        },
+      ],
+    },
+    {
+      deviceId: '1324654136',
+      deviceName: 'Device 2',
+      data: [
+        {
+          title: 'Angular',
+          downloadSize: 22.5,
+          uploadSize: 12.3,
+          upDate: '16/05/2023',
+          upTime: '3123',
+        },
+        {
+          title: 'React',
+          downloadSize: 22.5,
+          uploadSize: 12.3,
+          upDate: '16/05/2023',
+          upTime: '3123',
+        },
+      ],
+    },
+    {
+      deviceId: '1324654136',
+      deviceName: 'Device 3',
+      data: [
+        {
+          title: 'Angular',
+          downloadSize: 22.5,
+          uploadSize: 12.3,
+          upDate: '16/05/2023',
+          upTime: '3123',
+        },
+        {
+          title: 'React',
+          downloadSize: 22.5,
+          uploadSize: 12.3,
+          upDate: '16/05/2023',
+          upTime: '3123',
+        },
+      ],
+    },
+  ];
 }
--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent, CardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class HomeModule { }
