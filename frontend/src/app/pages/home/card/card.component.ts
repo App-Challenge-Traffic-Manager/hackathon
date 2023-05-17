20ef9ead30480b8313b986717464464b87ba379a
joaogoulartt <joaoogoulartt@gmail.com>
2023-05-17 16:43:12 -0300

    fix: :bug: fixes layout home

--- frontend/src/app/pages/home/card/card.component.ts
+++ frontend/src/app/pages/home/card/card.component.ts
@@ -4,11 +4,30 @@ import { IApplication } from 'src/app/interfaces/application.interface';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
   @Input()
-  public app!:IApplication;
+  public app!: IApplication;
 
-  constructor(){}
+  public regex = new RegExp('^([0-9])+(.)([0-9])+([A-z])([A-z])$');
+
+  testRegex(text: string) {
+    var response = this.regex.test(text);
+    return response;
+  }
+
+  formatValue(text: string) {
+    return this.testRegex(text)
+      ? text.substring(0, text.length - 2)
+      : text.substring(0, text.length - 1);
+  }
+
+  formatUnit(text:string) {
+    return this.testRegex(text)
+      ? text.substring(text.length - 2, text.length)
+      : text.substring(text.length - 1, text.length);
+  }
+
+  constructor() {}
 }
