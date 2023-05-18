163296d195c2f604e25469c26a7ad410d15c1fbd
Lucas Teles <devlucasteles@gmail.com>
2023-05-18 01:58:56 -0300

    feat: :sparkles: adds details page to frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { DetailsComponent } from './pages/details/details.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export enum Pages {
   Home = 'home',
+  Details = 'details',
 }
 
 const routes: Routes = [
@@ -16,12 +18,23 @@ const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: [{ path: Pages.Home, component: HomeComponent }],
+    children: [
+      { path: Pages.Home, component: HomeComponent },
+      {
+        path: Pages.Details,
+        component: DetailsComponent,
+      },
+    ],
   },
   {
     path: Pages.Home,
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: Pages.Details,
+    loadChildren: () =>
+      import('./pages/details/details.module').then((m) => m.DetailsModule),
   },
 ];
 
